c814622b93219a1853eb9303336fd58d8eec5747
NyeinChan <nyeinchansulwin.2011@gmail.com>
2019-08-16 22:21:51 +0630

    savethemoney

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,9 +33,15 @@ Rails.application.configure do
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
+  host = 'savethemoney.herokuapp.com' # ここをコピペすると失敗します。自分の環境に合わせてください。
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
+
+  
+  # Highlight code that triggered database queries in logs.
+  config.active_record.verbose_query_logs = true
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
